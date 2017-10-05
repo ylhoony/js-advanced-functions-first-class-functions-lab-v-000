3d3d092a42d372b667618b63b1ed3fb8735068c4
Hoon Lee <yl.hoony@gmail.com>
2017-10-05 06:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const returnLastTwoDrivers = (collection) => {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (num) => {
+function createFareMultiplier (num) => {
   return num * num;
 }
